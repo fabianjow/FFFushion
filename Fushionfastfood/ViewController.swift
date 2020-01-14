c7f4d28a25cd3ca2202a933eee41385d538a434b
Fabian <fabianjow@hotmail.com>
2020-01-14 16:07:04 +0800

    Initial Commit

---
+++ Fushionfastfood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fushionfastfood
+//
+//  Created by Fabian Jow on 14/1/20.
+//  Copyright © 2020 Fabian Jow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
