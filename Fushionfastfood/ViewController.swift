4d8f55b14611ef7f40f233fe0c332b886fd46452
adel <s10187871@connect.np.edu.sg>
2020-01-16 13:12:24 +0800

    Updated StoryBoard Login Page Added Content Storyboard Connected the login page to the content page

--- Fushionfastfood/ViewController.swift
+++ Fushionfastfood/ViewController.swift
@@ -10,10 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    
+    @IBAction func loginBtn(_ sender: Any) {
+        
+        if true {
+            let storyboard = UIStoryboard(name: "Content", bundle: nil)
+            let vc = storyboard.instantiateViewController(identifier: "Content") as UIViewController
+            vc.modalPresentationStyle = .fullScreen
+            present(vc, animated: true, completion: nil)
+            
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.  
     }
+    
 
 // test 1 
 }
