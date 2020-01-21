bcd375ae479f1e98401d180b7d6aeb2eb7feb409
adel <s10187871@connect.np.edu.sg>
2020-01-21 17:17:01 +0800

    Added Classes for food, cart, profile and stalls. Arranged them into grouped folders

---
+++ Fushionfastfood/Class/Food.swift
@@ -0,0 +1,9 @@
+//
+//  Food.swift
+//  Fushionfastfood
+//
+//  Created by Fabian on 21/1/20.
+//  Copyright © 2020 Fabian Jow. All rights reserved.
+//
+
+import Foundation
---
+++ Fushionfastfood/Class/Profile.swift
@@ -0,0 +1,9 @@
+//
+//  Profile.swift
+//  Fushionfastfood
+//
+//  Created by Fabian on 21/1/20.
+//  Copyright © 2020 Fabian Jow. All rights reserved.
+//
+
+import Foundation
