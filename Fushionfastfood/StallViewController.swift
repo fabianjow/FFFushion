78721f8336964e6612538e36f8aee916ab7e930d
adel <s10187871@connect.np.edu.sg>
2020-01-30 12:49:24 +0800

    Created the StallViewController

---
+++ Fushionfastfood/StallViewController.swift
@@ -0,0 +1,15 @@
+//
+//  StallViewController.swift
+//  Fushionfastfood
+//
+//  Created by Fabian on 30/1/20.
+//  Copyright © 2020 Fabian Jow. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class StallViewController: UIViewController{
+    
+    
+    
+}
