0fec379a4a1af23e1dd75e9b50ce742e4c35bc28
adel <s10187871@connect.np.edu.sg>
2020-01-30 12:25:07 +0800

    Edited the MapviewController and linked it to the mapview

--- Fushionfastfood/TableViewController/MapViewController.swift
+++ Fushionfastfood/TableViewController/MapViewController.swift
@@ -7,3 +7,63 @@
 //
 
 import Foundation
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    // call the locationdelegate
+    let locationDelegate = LocationDelegate()
+    var latestLocation: CLLocation? = nil
+    
+    let locationManager: CLLocationManager = {
+    $0.requestWhenInUseAuthorization()
+    $0.desiredAccuracy = kCLLocationAccuracyBest
+    $0.startUpdatingLocation()
+    $0.startUpdatingHeading()
+    return $0
+    }(CLLocationManager())
+    
+    
+    let regionRadius: CLLocationDistance = 250
+    func centerMapOnLocation(location: CLLocation)
+    {
+        let coordinateRegion = MKCoordinateRegion (
+            center: location.coordinate,
+            latitudinalMeters: regionRadius,
+            longitudinalMeters: regionRadius)
+        
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        functions
+        
+        
+//        locationdelegate
+        
+        //formatting the lat and long
+            locationManager.delegate = locationDelegate
+            locationDelegate.locationCallback = { location in
+                self.latestLocation = location
+                self.centerMapOnLocation(location: location)
+            
+                let annotation:MKPointAnnotation = MKPointAnnotation()
+                annotation.coordinate = location.coordinate
+                annotation.title = "Me!"
+                self.mapView.addAnnotation(annotation)
+                    
+                //self.mapView.showsUserLocation = true
+            
+        }
+        func gotostall()
+        {
+            
+        }
+        
+    }
+}
