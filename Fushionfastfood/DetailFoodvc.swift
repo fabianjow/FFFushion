27abd0d19972901810937230131e49fe361cfa02
adel <s10187871@connect.np.edu.sg>
2020-02-05 02:52:39 +0800

    sigh : content storyboard update

---
+++ Fushionfastfood/DetailFoodvc.swift
@@ -0,0 +1,30 @@
+//
+//  DetailFoodvc.swift
+//  Fushionfastfood
+//
+//  Created by Fabian on 5/2/20.
+//  Copyright © 2020 Fabian Jow. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetailFoodvc: UIViewController, UITableViewDelegate{
+
+
+    var getname = String()
+    var getamt = Int16()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+   
+    
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    
+    }
+}
