96dcfb0eb1748875a151f20c76a544448ddde07b
adel <s10187871@connect.np.edu.sg>
2020-02-07 15:26:09 +0800

    FINISHED PROFILE FINALLY LEFT EDITING AND UPLOAD PHOTO

--- Fushionfastfood/ProfileViewController.swift
+++ Fushionfastfood/ProfileViewController.swift
@@ -13,13 +13,28 @@ import FirebaseAuth
 
 class ProfileViewController: UIViewController {
     
+    @IBOutlet weak var userNameFld: UILabel!
+    @IBOutlet weak var emailFld: UILabel!
+    @IBOutlet weak var mobileNoFld: UILabel!
+    var dataDescription:Profile = Profile(username: "", email: "", mobileno: "")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let db = Firestore.firestore().collection("users").document("Test")
+        db.getDocument { (document, error) in
+            if let document = document, document.exists {
+                self.dataDescription = Profile(username: document.data()?["username"] as! String, email: document.data()?["email"] as! String, mobileno: document.data()?["mobileno"] as! String)
+                print (document.data()?["username"] as! String)
+            }
         
-        func logout() {
-            let storyboard = UIStoryboard(name: "Main", bundle: nil)
-            let ViewController = storyboard.instantiateViewController(identifier: "login")
-            present(ViewController,animated: true, completion: nil)
+            self.userNameFld.text = self.dataDescription.userName
+            self.emailFld.text = self.dataDescription.Email
+            self.mobileNoFld.text = self.dataDescription.mobileNo
+        
         }
+    }
+    
+    override func viewDidAppear(_ animated:Bool){
+        super.viewDidAppear(true)
     }
 }
